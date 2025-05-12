6cdf08b8ad724f7eeb81abbb745051b19c96516c
LuisTangarife <aux.reg.academicoii@autonoma.edu.co>
2025-05-11 20:16:26 -0500

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -181,9 +181,9 @@ function cargarGuia(tipo) {
 
     case "Guia-PDF":
       contenido = `
-        <iframe src="https://raw.githubusercontent.com/LuisTangarife/ChatBot/main/Condicinacadmica_2.pdf" 
+        <iframe src="https://github.com/LuisTangarife/ChatBot/blob/4776fa41a13a283164690dda74da4d6bb14af9e2/Condicinacadmica_2.pdf" 
                 width="100%" height="600px" style="border: none;"></iframe><br><br>
-        <a href="https://raw.githubusercontent.com/LuisTangarife/ChatBot/main/Condicinacadmica_2.pdf" 
+        <a href="https://github.com/LuisTangarife/ChatBot/blob/4776fa41a13a283164690dda74da4d6bb14af9e2/Condicinacadmica_2.pdf" 
            download 
            style="background: #4CAF50; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px;">
           📥 Descargar Guía en PDF
